3b443f77ca7ef5886d3b4f017cf3f7979e0570ff
sakibtamim <sakib.tamim@gmail.com>
2023-07-04 12:52:49 +0600

    first commit

--- clock.py
+++ clock.py
@@ -2,3 +2,9 @@ import datetime
 
 date = datetime.datetime.now()
 print("It's ", date)
+print(date.year)
+print(date.month)
+print(date.strftime("%A"))
+print(date.day)
+print(date.ctime())
+print(date.hour)
